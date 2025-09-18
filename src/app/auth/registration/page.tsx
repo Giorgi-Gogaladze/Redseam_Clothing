9947c542b169eaee9ec9537e262c9d3b26097035
GiorgiGogaladze <gogaladzegio55@gmail.com>
2025-09-18 16:11:59 +0400

    feat: adding image choose/remove funcitonality

--- src/app/auth/registration/page.tsx
+++ src/app/auth/registration/page.tsx
@@ -1,11 +1,21 @@
+'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Button from '@/components/reusabel_components/Button'
 import Link from 'next/link'
-import UserImage from '@/../public/images/userImage.jpg'
 import Input from '@/components/reusabel_components/Input'
 
 const page = () => {
+  const [userImage, setUserImage] = useState<string | null>(null);
+  const imageRef = useRef<HTMLInputElement | null>(null);
+
+  const handleImageShow = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.files?.length){
+      const image = e.target.files[0];
+      const imageUrl = URL.createObjectURL(image);
+      setUserImage(imageUrl) 
+    }
+  }
   
   return (
     <div className='flex flex-col gap-12 absolute top-[152px] left-[173px] w-[554px]'>
@@ -13,20 +23,51 @@ const page = () => {
       Registration
     </h1>
       <div className='w-[554px] h-[518px] flex flex-col gap-[46px]'>
+        { userImage ? (
         <div className='w-[272px] h-[100px] flex gap-[15px] items-center'>
           <div className='w-[100px] h-[100px] rounded-full overflow-hidden flex items-center justify-center'>
             <Image
-              src={UserImage}
+              src={userImage}
               alt='user image'
               width={1000}
               height={1000}
               className='object-cover w-full h-full '/>
           </div>
           <div className='w-[157px] h-[21px] flex gap-[15px] justify-center items-center  font-normal text-[14px] tracking-[0]  leading-[16px] text-[var(--dark-blue-2)]'>
-            <h1 className='cursor-pointer whitespace-nowrap'>Upload new</h1>
-            <h1 className='cursor-pointer'>Remove</h1>
+            <h1 
+            onClick={() => imageRef.current?.click()}
+            className='cursor-pointer whitespace-nowrap'>Upload new</h1>
+            <h1
+            onClick={() => setUserImage(null)} 
+            className='cursor-pointer'>Remove</h1>
+          </div>
+          <input 
+            type="file" 
+            ref={imageRef}
+            accept='image/*'
+            onChange={handleImageShow}
+            className='hidden' />
+        </div>
+        ) : (
+          <div className='w-[272px] h-[100px] flex gap-[15px] items-center'>
+          <div 
+          onClick={() => imageRef.current?.click()}
+          className='w-[100px] h-[100px] rounded-full overflow-hidden border border-[var(--grey-2)] flex items-center justify-center cursor-pointer'>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11.9297 3C12.5984 3.00002 13.2228 3.33424 13.5938 3.89062L14.4062 5.10938C14.7772 5.66576 15.4016 5.99998 16.0703 6H17C18.1046 6 19 6.89543 19 8V15C19 16.1046 18.1046 17 17 17H3C1.89543 17 1 16.1046 1 15V8C1 6.89543 1.89543 6 3 6H3.92969C4.59837 5.99998 5.22283 5.66576 5.59375 5.10938L6.40625 3.89062C6.77717 3.33424 7.40163 3.00002 8.07031 3H11.9297ZM10 6.5C7.51472 6.5 5.5 8.51472 5.5 11C5.5 13.4853 7.51472 15.5 10 15.5C12.4853 15.5 14.5 13.4853 14.5 11C14.5 8.51472 12.4853 6.5 10 6.5ZM10 8C11.6569 8 13 9.34315 13 11C13 12.6569 11.6569 14 10 14C8.34315 14 7 12.6569 7 11C7 9.34315 8.34315 8 10 8Z" fill="#3E424A"/>
+            </svg>
+          </div>
+          <div className='w-[157px] h-[21px] flex gap-[15px] justify-start items-center  font-normal text-[14px] tracking-[0]  leading-[16px] text-[var(--dark-blue-2)]'>
+            <h1>Upload image</h1>
+            <input 
+            type="file" 
+            ref={imageRef}
+            accept='image/*'
+            onChange={handleImageShow}
+            className='hidden' />
           </div>
         </div>
+        )}
 
         <div className='w-[554px] h-[240px] flex flex-col gap-[24px]'>
           <Input width={554} placeholder='Username *' type={'text'} />
